66bc8b582a34081dc4b89de5db7260a462a609a7
hibukki <myselfandfredy@gmail.com>
2022-09-10 14:22:10 +0300

    Main page: Nicer - No lorem ipsum - Add "about" with cute cats

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,36 +16,52 @@ const Home: NextPage = () => {
       </Head>
       <div className="container mx-auto max-w-screen-lg p-4">
         <Navbar />
-        <main className="container mx-auto flex min-h-screen flex-col justify-center p-4">
+        <main className="container mx-auto flex min-h-screen flex-col justify-center gap-4 p-4">
           <h1 className="text-3xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
             Effective Developers
           </h1>
-          <img src="https://placekitten.com/300/300" />
           <section className="">
-            <h2 className="text-2xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
-              What we do?
-            </h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-              alias assumenda blanditiis culpa debitis dicta, dolor ipsam
-              molestiae, nesciunt porro quod tempora voluptates. Aliquam esse ex
-              laboriosam quod recusandae veniam.
+              We help software developers and people running software projects
+              in the Effective Altruism community
             </p>
           </section>
-          <section className="">
+          <section>
             <h2 className="text-2xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
               About Us
             </h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-              alias assumenda blanditiis culpa debitis dicta, dolor ipsam
-              molestiae, nesciunt porro quod tempora voluptates. Aliquam esse ex
-              laboriosam quod recusandae veniam.
+              We're both experienced developers (10+ years of professional
+              experience each), here to try helping the EA community however we
+              can.
             </p>
+            {/* <h3>
+              "About us" - autocompleted by copilot
+            </h3>
+            <p>
+              We are a group of software developers and people running software projects in the Effective Altruism community. We are passionate about using technology to help people and the world. We are also passionate about helping people in the Effective Altruism community to use technology to help people and the world.
+            </p> */}
           </section>
-          <div className="flex w-full items-center justify-center pt-6 text-2xl text-blue-500">
-            {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
-          </div>
+          <section>
+            <nav className="">
+              <ul className="flex items-center justify-center gap-10 text-center font-bold transition-colors">
+                <li>
+                  <img
+                    className="rounded-full"
+                    src="https://placekitten.com/256/256"
+                  />
+                  <p>Soof Golan</p>
+                </li>
+                <li className="">
+                  <img
+                    className="rounded-full"
+                    src="https://placekitten.com/256/256"
+                  />
+                  <p>Yonatan Cale</p>
+                </li>
+              </ul>
+            </nav>
+          </section>
         </main>
         <Footer />
       </div>
