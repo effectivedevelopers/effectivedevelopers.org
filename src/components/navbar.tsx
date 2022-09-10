5591efa0e5c5d955521c4f2aa9ccd521e5a7c353
hibukki <myselfandfredy@gmail.com>
2022-09-10 14:34:43 +0300

    Navbar: More page plans (broken)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,23 +9,32 @@ export const Navbar: NextComponentType = () => (
       <ul className="flex items-center gap-4 font-bold transition-colors">
         <li>
           <Link href="/job-board">
-            <a className="hover:text-cyan-600">
-              Job Boards
-            </a>
+            <a className="hover:text-cyan-600">Job Boards</a>
           </Link>
         </li>
         <li>
-          <Link href="/job-board">
-            <a className="hover:text-cyan-600">
-              Help me hire
-            </a>
+          <Link href="/hiring-support">
+            <a className="hover:text-cyan-600">Help me hire</a>
           </Link>
         </li>
         <li>
-          <Link href="/job-board">
-            <a className="hover:text-cyan-600">
-              Help me hire
-            </a>
+          <Link href="/about">
+            <a className="hover:text-cyan-600">About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a className="hover:text-cyan-600">Help find new effective orgs</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a className="hover:text-cyan-600">Pitch to EA CTOs</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a className="hover:text-cyan-600">Dev work</a>
           </Link>
         </li>
 
